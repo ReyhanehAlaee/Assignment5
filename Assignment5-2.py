1c794f23287b16a626fe41a4a57dfa742fbc7384
ReyhanehAlaee <148444766+ReyhanehAlaee@users.noreply.github.com>
2023-11-08 18:47:05 +0330

    Update Assignment5-2.edit.py

--- Assignment5-2.py
+++ Assignment5-2.py
@@ -1,9 +1,11 @@
+import math
 row = int(input("Enter numbers of rows :"))
 col = int(input("Enter numbers of cols :"))
 
 for i in range(row):
     for j in range(col):
-        
-            print("#*", end="")
-
+        if (i + j) %2 == 0:
+            print("#", end="")
+        else:
+            print("*", end="")
     print()
